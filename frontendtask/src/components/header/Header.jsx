7752aefc10f4995ca829e323cef7fd3f8a10c0c9
Nicksheinman <nicksheinman@gmail.com>
2025-03-12 21:27:24 +0200

    test

--- frontendtask/src/components/header/Header.jsx
+++ frontendtask/src/components/header/Header.jsx
@@ -1,13 +1,18 @@
 
 
+import { useState } from "react";
 import "./header.css"
 
 export default function Header() {
     const date=Date.now()
+    const [dateS, setDate]=useState(date);
+    setInterval(() => {
+     setDate(Date.now())
+    }, 500);
     return (
       <header>
         <div className="headerName"><h1>TaskFrenzy</h1></div>
-        <div className="HeaderSecond"><span>time={date.toLocaleString()}</span></div>
+        <div className="HeaderSecond"><span>time={dateS}</span></div>
       </header>
     );
 }
